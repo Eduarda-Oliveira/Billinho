e9a3814ecee65c3680d03e20ffe6eb98eb9dd7fe
Maria Oliveira <duda.oliveira.1503@gmail.com>
2020-12-11 17:35:18 -0300

    tentando criar fatura

--- app/controllers/api/v1/matriculas_controller.rb
+++ app/controllers/api/v1/matriculas_controller.rb
@@ -1,7 +1,7 @@
 module Api
 	module V1
         class Api::V1::MatriculasController < ApplicationController   
-            include criaFatura
+            #include criaFatura
             # Listar todas as matriculas
             def index
                 matriculas = Matricula.order('created_at DESC');
@@ -22,6 +22,11 @@ module Api
                 end
             end
              # parametros aceitos matricula
+             after_action:fatura
+
+             def fatura
+                fatura = createFatura.new(fatura_params).perform
+            end
              private
             def matricula_params
                 params.permit(:valor_total_reais, :quantidade_faturas, :dia_vencimento_faturas, :nome_curso, :aluno_id, :instituicao_id) 
@@ -30,11 +35,47 @@ module Api
             def valorFatura
                 valor_total_reais/ quantidade_faturas 
             end    
-             
-            after_create :criaFatura.new(fatura_params).perform
-            end
+
+
             def fatura_params
-                params.permit(matricula: matricula, matricula.dia_vencimento_faturas, matricula.quantidade_faturas, valorFatura) 
+                params.permit(matricula :matricula, matricula.dia_vencimento_faturas, matricula.quantidade_faturas, valorFatura) 
+            end
+
+
+            #prepara os atricutos da classe
+            def initialize(params={})
+                @matricula = params[:matricula]
+                @dia_vencimento = params[:dia_vencimento_faturas]
+                @valor = params[:valorFatura]
+                @quantidade = params[:quantidade_faturas]
+            end
+
+            def perform
+                createFatura
+            end
+
+            def statusDefault
+                'Aberta'
+            end
+
+            def validaData
+                data = Date.today
+                dia = data.mday
+                return dia_vencimento > dia
+            end 
+
+            def dataInicio
+                if validaData
+                    Date.new(Date.today.cwyear,Date.today.mon, dia_vencimento)
+                else
+                    Date.new(Date.today.cwyear,Date.today.mon, dia_vencimento).next_month
+                end
+            end  
+
+            def createFatura
+                #@quantidade.times do 
+                matricula.faturas.create(valor_fatura_reais: valor, data_vencimento: dataInicio, status: statusDefault, matricula_id: matricula)
+                #end
             end
 		end
 	end
